1017c9f0e9883dfa25b3b847f9a7986b59b64199
seungmin.bang <seungmin.bang@vtw.co.kr>
2024-08-05 16:10:55 +0900

    Feat: board CRUD 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { UserModule } from "./user/user.module";
+import { BoardsModule } from "./boards/boards.module";
 
 @Module({
-  imports: [UserModule],
+  imports: [BoardsModule],
   controllers: [AppController],
   providers: [AppService]
 })
---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
+
+@Controller('boards')
+export class BoardsController {
+
+  constructor(private boardsService: BoardsService) {
+  }
+
+  @Get()
+  getAllBoards(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardsService.getBoardById(id);
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardsService.createBoard(createBoardDto);
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(@Param('id') id: string, @Body('status') status: BoardStatus) {
+    return this.boardsService.updateBoardStatus(id, status);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id') id: string): void {
+    return this.boardsService.deleteBoard(id);
+  }
+}
+
